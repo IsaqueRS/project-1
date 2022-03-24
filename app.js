d31578ad7863bff0a62564988e99520ca3b94e07
Isaque Roberto <isaqueroberto22@gmail.com>
2022-03-24 18:18:23 -0300

    att-simples

--- app.js
+++ app.js
@@ -6,13 +6,29 @@
     const admin = require('./routes/admin')
     const path = require('path')
     const mongoose = require('mongoose')
+    const session = require('express-session')
+    const flash = require('connect-flash')
+    const moment = require('moment')
 //configurações
+    //sessão
+    app.use(session({
+        secret: "cursodenode",
+        resave: true,
+        saveUninitialized: true
+    }))
+    app.use(flash())
+    //middleware
+        app.use((req, res, next) => {
+            res.locals.success_msg = req.flash('success_msg')
+            res.locals.error_msg = req.flash('error_msg')
+            next()
+        })
     //body parser
-    app.use(bodyParser.urlencoded({extended: true}))
-    app.use(bodyParser.json())
+        app.use(bodyParser.urlencoded({extended: true}))
+        app.use(bodyParser.json())
     //Handlebars
-    app.engine('handlebars', handlebars.engine({defautLayout: 'main'}))
-    app.set('view engine', 'handlebars');
+        app.engine('handlebars', handlebars.engine({defautLayout: 'main', helpers: { formatDate: (date) => { return moment(date).format('DD/MM/YYYY HH:mm')}}}))
+        app.set('view engine', 'handlebars');
     //moongose
         mongoose.connect('mongodb://localhost/blogapp').then((req, res) => {
             console.log('Conectado c o Mongo')
@@ -21,6 +37,7 @@
         })
     //Public
         app.use(express.static(path.join(__dirname,'public')))
+
 //rotas
     app.get('/', (req, res) => {
         res.send('page principal')
--- routes/admin.js
+++ routes/admin.js
@@ -13,7 +13,12 @@ router.get('/posts', (req, res) => {
 })
 
 router.get('/categorias', (req, res) => {
-    res.render('admin/categorias')
+    Categoria.find().sort({date:'desc'}).lean().then((categorias) => {
+        res.render('admin/categorias', {categorias: categorias})
+    }).catch((err) => {
+        req.flash('error_msg', 'Houve um erro ao listar as categorias')
+        res.redirect('/admin')
+    })  
 })
 
 router.get('/categorias/add', (req, res) => {
@@ -21,16 +26,40 @@ router.get('/categorias/add', (req, res) => {
 })
 
 router.post('/categorias/nova', (req, res) => {
-    const novaCategoria = {
-        nome: req.body.nome,
-        slug: req.body.slug
+    
+    var erros = []
+
+    if(!req.body.nome || typeof req.body.nome == undefined  || req.body.nome == null){
+        erros.push({texto: "Nome Inválido"})
     }
 
-    new Categoria(novaCategoria).save().then(() => {
-        console.log('Categoria salva com Sucesso!')
-    }).catch((err) => {
-        console.log('Erro ao salvar categoria', err);
-    })
-})
+    if(!req.body.slug && typeof req.body.slug == undefined  || req.body.slug == null){
+        erros.push({texto: "Slug Inválido"})
+    }
 
+    if(req.body.nome.length < 2){
+        erros.push({texto: "Digite um nome maior"})
+    }
+
+    if(erros.length > 0){
+        res.render("admin/addcategorias", {erros: erros})
+    }else{
+        const novaCategoria = {
+            nome: req.body.nome,
+            slug: req.body.slug
+        }
+    
+        new Categoria(novaCategoria).save().then(() => {
+            req.flash('success_msg', 'Categoria criada com sucesso')
+            res.redirect('/admin/categorias')
+        }).catch((err) => {
+            req.flash('error_msg', 'houve um erro ao cadastrar a categoria, tente novamente!')
+            res.redirect('/admin')
+        })
+    
+    }
+
+    
+   
+})
 module.exports = router
